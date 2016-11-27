33dad237eaa39d5e5853f398a5c18ebdf5810a4b
Bowers96 <bowersc96@gmail.com>
2016-11-27 16:16:04 -0500

    Create a show/hide dropdown menu

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,37 +1,11 @@
 'use strict';
 
-let template = function(text) {
-  return '<p><input type="checkbox"><i class="glyphicon glyphicon-star"></i><span>' + text + '</span><i class="glyphicon glyphicon-remove"></i></p>';
-};
-
-let add = function(item) {
-  let html = template(item);
-  $(html).appendTo('.list');
-};
-
 let main = function() {
-  $('form').submit(function() {
-    let todo = $('#todo');
-    if (todo.val() !== "") {
-      let html = template(todo.val());
-      $(html).appendTo('.list');
-      $(todo).val("");
-    }
-    return false;
+  $('img').click(function() {
+    $('.dropdown-menu').show(500).hide();
   });
-
-  $(document).on("click", '.glyphicon-remove', function() {
-    $(this).parent().remove();
-  });
-
-
-  if (annyang) {
-    let commands = {
-      'add *item': add,
-    };
-    annyang.addCommands(commands);
-    annyang.start();
-  }
+  return false;
 };
+
 
 $(document).ready(main);
