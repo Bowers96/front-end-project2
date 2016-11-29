2edf56162eab32e5850241365fd934a9721bece8
Bowers96 <bowersc96@gmail.com>
2016-11-29 11:18:19 -0500

    Add html and css and jQuery

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,8 +4,14 @@ let main = function() {
   $('img').click(function() {
     $('.dropdown-menu').show(500).hide();
   });
-  return false;
+
+  $(".btn").click(function() {
+    let item = {
+      title: $('#title').val(),
+      category: $('#category').val(),
+      description: $('#description').val()
+    }
+    $(".side-content-right").append(`<div class="contents">${item.title}</div><div class="contents">${item.category}</div><div class="contents">${item.description}</div>`);
+  });
 };
-
-
 $(document).ready(main);
