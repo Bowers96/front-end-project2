5cb3874b5676f18a9b9bb788b15eacfe1c9ec9fa
Bowers96 <bowersc96@gmail.com>
2016-11-30 11:08:59 -0500

    getting master back on track

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,8 +2,9 @@
 
 let main = function() {
   $('img').click(function() {
-    $('.dropdown-menu').show(500).hide();
+    $('.dropdown-menu').slideToggle();
   });
+
 
   $(".btn").click(function() {
     let item = {
@@ -11,7 +12,41 @@ let main = function() {
       category: $('#category').val(),
       description: $('#description').val()
     }
-    $(".side-content-right").append(`<div class="contents">${item.title}</div><div class="contents">${item.category}</div><div class="contents">${item.description}</div>`);
+    $(".side-content-right").append(`<div class="item"><div class="contents">${item.title}</div><div class="contents">${item.category}</div><div class="contents">${item.description}</div></div>`);
   });
 };
+
+
+$("#auth-one").click(function() {
+  $('.frame').hide()
+  $('.sign-up').show();
+});
+
+$(".btn-one").click(function() {
+  $('.frame').hide();
+  $('.sign-in').show();
+});
+
+$("#auth-two").click(function() {
+  $('.frame').hide();
+  $('.sign-in').show();
+});
+
+$(".btn-two").click(function() {
+  $('.frame').hide();
+  $('.main').show();
+});
+
+$("#auth-three").click(function() {
+  $('.frame').hide();
+  $('.change-password').show();
+});
+
+$("#auth-four").click(function() {
+  $('.frame').hide();
+  $('.sign-out').show();
+});
+
+
+
 $(document).ready(main);
